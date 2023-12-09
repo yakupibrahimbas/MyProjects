1f6f63c164384a6f23ae70009dece008cd175ca3
yakupibrahimbas <ibrahimbasyakup@gmail.com>
2023-12-09 23:31:29 +0300

    a9

---
+++ src/main/java/A9/For03.java
@@ -0,0 +1,2 @@
+package A9;public class For03 {
+}
--- src/main/java/A9/TersUcgen.java
+++ src/main/java/A9/TersUcgen.java
@@ -1,2 +1,25 @@
-package A9;public class TersUcgen {
+package A9;
+
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("kac satir ucgen olsun");
+        int number= input.nextInt();
+
+        for (int i=1;i<=number;i++){
+            for (int j=1;j<=(i-1);j++){
+                System.out.println(" ");
+            }
+            int starCount=(number+1)-i;
+            System.out.println(number-(i+1));
+        }
+
+
+
+
+
+    }
 }
